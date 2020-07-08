54781bd3617ff2f7c4bba4c432449d825505d6af
seenuvenkat <67901425+seenuvenkat@users.noreply.github.com>
2020-07-08 19:29:06 +0530

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -1,16 +1,16 @@
 def binary_search(item_list,item):
-	first = 0
-	last = len(item_list)-1
+	f = 0
+	l = len(item_list)-1
 	found = False
-	while( first<=last and not found):
-		mid = (first + last)//2
-		if item_list[mid] == item :
+	while( f<=l and not found):
+		m = (f + l)//2
+		if item_list[m] == item :
 			found = True
 		else:
-			if item < item_list[mid]:
-				last = mid - 1
+			if item < item_list[m]:
+				l = m - 1
 			else:
-				first = mid + 1	
+				f = m + 1	
 	return found
 	
 print(binary_search([1,2,3,5,8], 6))
